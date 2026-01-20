06fef838dd62ea4c4c4b1f4b52b97ce858ddfcd6
JH Kim <rlawogus65@gmail.com>
2026-01-20 22:57:58 +0900

    [BOJ]_1515_수 이어 쓰기

---
+++ January/Week3/Jaehyeon/S2_1515.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class S2_1515 {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String num = br.readLine();
+
+        int point = 0;
+        int base = 0;
+
+        while (true) {
+            base++;
+            String tmp = String.valueOf(base);
+
+            for (int i = 0; i < tmp.length(); i++) {
+                if (tmp.charAt(i) == num.charAt(point)) {
+                    point++;
+                    if (point == num.length()) {
+                        System.out.println(base);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
